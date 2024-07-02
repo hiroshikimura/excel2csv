9f86c3fc7c5dc90aee0c00aafc7a03af01fab5fd
hiroshikimura <2766653+hiroshikimura@users.noreply.github.com>
2024-07-02 09:51:17 +0900

    initial commit

---
+++ excel2csv.rb
@@ -0,0 +1,50 @@
+# frozen_string_literal: true
+
+require 'bundler/inline'
+
+gemfile(true) do
+  source 'https://rubygems.org'
+
+  git_source(:github) { |repo| "https://github.com/#{repo}.git" }
+
+  gem 'roo'
+  gem 'base64'
+  gem 'csv'
+  gem 'optparse'
+end
+
+require 'roo'
+require 'optparse'
+
+def output_csv(sheet, sheet_name)
+  puts "processing #{sheet}"
+  header = (list = sheet.entries.to_a).shift.map{ |e| e.gsub /\s+/, '' }.map { |str| str.unicode_normalize(:nfkc)}
+  CSV.open("#{sheet_name}.csv", "w") do |csv|
+    csv << header
+    list.each { |r| csv << r.map { |str| str.to_s.unicode_normalize(:nfkc)} }
+  end
+end
+
+args = ARGV.getopts(nil, 'in:', 'sheets:').transform_keys!(&:to_sym).map do |k, v|
+  [
+    k,
+    {
+      in: ->(value) { value },
+      sheets: ->(value) { value.split(',') }
+    }[k].call(v),
+  ]
+end.to_h
+
+wb = Roo::Excelx.new(args[:in])
+# puts wb.sheets.join(',')
+
+# binding.irb
+
+wb.sheets.each do |sh|
+  begin
+    output_csv(wb.sheet(sh), sh) if args[:sheets].include?(sh) || args[:sheets].include?('--')
+  rescue => e
+    binding.irb
+    raise e
+  end
+end
